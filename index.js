4811b96d1ebea94f62a2c22cb2af470cf0be4e09
Ulric <ulricaird1@gmail.com>
2024-05-31 20:21:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const buttons = document.getElementsByClassName("myButton")
+const buttons = document.getElementsByClassName("navbar")
 
 for (const button of buttons) { // Iterate buttons with this class name
     button.addEventListener(
         "click", (e) => {
-            console.log(e.target.value)
+            console.log("Clicked Test!")
         }
     )
 }
